eded9b5b42823c41b13371da429d7529a54f217c
SOIX <sasa@infinity-x.tech>
2024-02-13 03:35:48 +0100

    Update vendor path

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
 session_start();
 
 use App\Core\Router;
-
-require 'vendor/autoload.php';
 
 // Load environment variables from .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
