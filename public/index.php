6ff2c5117fa2997643f1c08a5af72753ba61a627
SOIX <sasa@infinity-x.tech>
2024-02-13 03:38:57 +0100

    Update env path

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ session_start();
 use App\Core\Router;
 
 // Load environment variables from .env file
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 // Generate routes
